a668b3e31138f6934f8f3bd3c2dae77285660e06
Dev <devsherkhane6@gmail.com>
2026-02-13 15:50:21 +0530

    SMTP server added

--- consumer.go
+++ consumer.go
@@ -2,15 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/smtp"
 	"sync"
+	"time"
 )
 
 func emailWorker(id int, ch chan Recipent, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for recipent := range ch {
-		
-		fmt.Println(id, recipent)
+		smtpHost := "localhost"
+		smtpPort := "1025"
+
+		formattedMsg := fmt.Sprintf("To: %s\r\nSubject: Test Email\r\n\r\n%s\r\n", recipent.Email, "Just testing our email campaign")
+		msg := []byte(formattedMsg)
+
+		fmt.Printf("Worker %d: Sending email to %s \n", id, recipent.Email)
+
+		err := smtp.SendMail(smtpHost+":"+smtpPort, nil, "devsherkhane6@gmail.com", []string{recipent.Email}, msg)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		time.Sleep(50 * time.Millisecond)
+		fmt.Printf("Worker %d: Sent email to %s \n", id, recipent.Email)
+
 	}
 
 }
